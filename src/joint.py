505c96e1a272ceeaacbfd937e691fb6a81c29d8e
Sean <sm0077@uah.edu>
2019-09-19 15:36:14 -0500

    Added link.py. Added joint.py.

---
+++ src/joint.py
@@ -0,0 +1,6 @@
+# source file for joint class
+
+class Joint:
+
+    def __init__(self):
+        self.exists = True
--- src/link_robotics_toolkit.py
+++ src/link_robotics_toolkit.py
@@ -1,6 +1,6 @@
 # source file for link_robotics_toolkit
 
-class LRT:
+class Link_Robotics_Toolkit:
 
     def __init__(self):
         self.exists = True
