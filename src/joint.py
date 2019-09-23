1cc0240ac7ebeb6c72a4f1b851114b77aba3cf5e
Sean Mitchell <sm0077@uah.edu>
2019-09-23 15:17:09 -0500

    Added position member variables for links and joints.

--- src/joint.py
+++ src/joint.py
@@ -6,10 +6,31 @@ class Joint:
         self.exists = True
         self.joint_type = ""
         self.joint_angle = 0
+        self.x = 0
+        self.y = 0
+        self.z = 0
 
     def Get_Joint_Type(self):
         return self.joint_type
 
     def Set_Joint_Type(self, joint_type):
         self.joint_type = joint_type
+        return 0
+
+    def Set_x(self, x):
+        return 0
+
+    def Get_x(self, x):
+        return 0
+
+    def Set_y(self, x):
+        return 0
+
+    def Get_y(self, y):
+        return 0
+
+    def Set_z(self, x):
+        return 0
+
+    def Get_z(self, z):
         return 0
--- src/link.py
+++ src/link.py
@@ -6,3 +6,24 @@ class Link:
         self.exists = True
         self.link_length = 0
         self.link_twist = 0
+        self.x = 0
+        self.y = 0
+        self.z = 0
+
+    def Set_x(self, x):
+        return 0
+
+    def Get_x(self, x):
+        return 0
+
+    def Set_y(self, x):
+        return 0
+
+    def Get_y(self, y):
+        return 0
+
+    def Set_z(self, x):
+        return 0
+
+    def Get_z(self, z):
+        return 0
